3084416defa54db05027424ae629f9e9f7208f13
vadim50 <waswas4@yandex.ru>
2017-02-06 16:56:03 +0300

    Add a full_tille error

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,21 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
-  
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get :home
+    get static_pages_home_url
     assert_response :success
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get :help
+    get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get :about
+    get static_pages_about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get contact" do
-    get :contact
-    assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
